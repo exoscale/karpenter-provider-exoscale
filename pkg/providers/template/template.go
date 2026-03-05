c6b78a5afe902d9be354d8957074fd1fd7b9e2dc
Philippe Chepy <philippe.chepy@exoscale.ch>
2026-03-05 09:03:00 +0100

    chore: remove unused template.Client interface

--- pkg/providers/template/template.go
+++ pkg/providers/template/template.go
@@ -1,16 +1,9 @@
 package template
 
 import (
-	"context"
-
 	egov3 "github.com/exoscale/egoscale/v3"
 	v1 "k8s.io/api/core/v1"
 )
-
-// Client is an interface for getting templates from Exoscale
-type Client interface {
-	GetTemplate(ctx context.Context, id egov3.UUID) (*egov3.Template, error)
-}
 
 type Template struct {
 	ID     string
